14e0d4fc86f432e3a33c493338e3b79a85c5fb50
Jiawen Chen <sophiachen1130@gmail.com>
2018-08-01 16:45:16 -0500

    Initial commit

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
 // Select size input
-
+let height,width;
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event){
+  event.preventDefault();
+  height=$('#inputHeight').val();
+  width=$('#inputWidth').val();
+  makeGrid(height, width);
+});
 
-function makeGrid() {
+function makeGrid(x,y) {
+  $('tr').remove();
 
 // Your code goes here!
-
-}
+  for (let i=1; i<=x; i++){
+    $('#pixelCanvas').append('<tr id=row'+i+'></tr>');
+    for (let j=1; j<=y; j++){
+      $('#row'+i).append('<td></td>');
+    }
+  }
+  //add color to cell
+  $('td').click(function addColor(){
+    color=$('#colorPicker').val();
+    if ($(this).attr('style')){
+       $(this).removeAttr('style')
+    }else{
+     $(this).attr('style','background-color:'+color);
+    }
+});
+};
